464035d8641c32cedf087677820377ae97447e04
Tyler Farghly <tylerf98@live.co.uk>
2019-01-26 17:27:06 +0000

    fixed problem

--- feature-detection/example.py
+++ feature-detection/example.py
@@ -1,15 +1,16 @@
 import cv2
-import eye
+import matplotlib.pyplot as plt
+import ffdet
 
 # example
 capture = cv2.VideoCapture(0)
-facial_features_list, image = eye.get_facial_features_from_capture(capture)
+facial_features_list, image = ffdet.get_facial_features_from_capture(capture)
 
 if facial_features_list:
 	features = facial_features_list[0]
-	image = eye.draw_features(image, [features["face"]], (255, 0, 0))
-	image = eye.draw_features(image, [features["left_eye"]], (0, 255, 0))
-	image = eye.draw_features(image, [features["right_eye"]], (0, 0, 255))
+	image = ffdet.draw_features(image, [features["face"]], (255, 0, 0))
+	image = ffdet.draw_features(image, [features["left_eye"]], (0, 255, 0))
+	image = ffdet.draw_features(image, [features["right_eye"]], (0, 0, 255))
 else:
 	print("no features found!")
 
