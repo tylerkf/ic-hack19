dfbe35abbca890e5f1910d2f2e23743a088d9dfa
Tyler Farghly <tylerf98@live.co.uk>
2019-01-27 04:39:57 +0000

    Added Server

--- feature-detection/example.py
+++ feature-detection/example.py
@@ -8,11 +8,14 @@ facial_features_list, image = ffdet.get_facial_features_from_capture(capture)
 
 if facial_features_list:
 	features = facial_features_list[0]
-	image = ffdet.draw_features(image, [features["face"]], (255, 0, 0))
-	image = ffdet.draw_features(image, [features["left_eye"]], (0, 255, 0))
-	image = ffdet.draw_features(image, [features["right_eye"]], (0, 0, 255))
+	image_features = image.copy()
+	image_features = ffdet.draw_features(image_features, [features["face"]], (255, 0, 0))
+	image_features = ffdet.draw_features(image_features, [features["left_eye"]], (0, 255, 0))
+	image_features = ffdet.draw_features(image_features, [features["right_eye"]], (0, 0, 255))
+
+	plt.figure()
+	plt.imshow(image_features, cmap='gray')
+
+	plt.show()
 else:
 	print("no features found!")
-
-plt.imshow(image)
-plt.show()
