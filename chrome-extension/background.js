506c1e3a041a2621b1a44f79bbbb946d08722a95
Tyler Farghly <tylerf98@live.co.uk>
2019-01-27 11:44:51 +0000

    Added alot

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,25 +1,26 @@
-const STREAM_URL = "http://cyclopsx.duckdns.org/stream";
+const STREAM_URL = "http://cyclopsx.duckdns.org/stream/";
+const SOCKET_URL = "http://cyclopsx.duckdns.org:8810";
 var streamTabId = null;
+var uid = null;
+var socket = null;
+
+var lastAction = {}
 
 chrome.extension.onConnect.addListener(function(port) {
   console.log("Connected .....");
   port.onMessage.addListener(function(msg) {
     if (msg == "start") {
       console.log("startd");
-
-      chrome.tabs.create({url: STREAM_URL, active: false}, function(tab) {
-        streamTabId = tab.id;
-        console.log(streamTabId);
-        port.postMessage("started");
-      });
-
+      startSockets();
     } else if (msg == "stop") {
+      stopSockets();
       console.log("stop");
       chrome.tabs.remove(streamTabId, function() {
         streamTabId = null;
         port.postMessage("stopped");
       });
     } else if (msg == "status") {
+        port.postMessage("started");
       if (streamTabId !== null) {
         port.postMessage("started");
       } else {
@@ -34,6 +35,33 @@ chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
     console.log("dangit");
   }
 });
+
+function stopSockets() {
+  if (socket != null) {
+    socket.disconnect()
+  }
+}
+
+function startSockets() {
+  socket = io(SOCKET_URL);
+
+  socket.on('action', function(msg) {
+    console.log(msg);
+    try {
+      lastAction = JSON.parse(msg);
+    } catch(e) {
+      console.log(e);
+    }
+  });
+
+  socket.on('uid', function(msg) {
+    uid = msg;
+    chrome.tabs.create({url: STREAM_URL + uid, active: false}, function(tab) {
+        streamTabId = tab.id;
+        console.log(streamTabId);
+      });
+  });
+}
 
 function runit() {
   chrome.tabs.query({active: true}, function(tabs){
