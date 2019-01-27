a1d6fabcb2aeb9d1f38e3a5dec207af2786d161d
Peter Whalley <peter.a.whalley@gmail.com>
2019-01-27 11:52:21 +0000

    SUBMIT

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,36 +1,80 @@
 const STREAM_URL = "http://cyclopsx.duckdns.org/stream";
+
 var streamTabId = null;
 
+
 chrome.extension.onConnect.addListener(function(port) {
+
   console.log("Connected .....");
+
   port.onMessage.addListener(function(msg) {
+
     if (msg == "start") {
+
       console.log("startd");
 
+
       chrome.tabs.create({url: STREAM_URL, active: false}, function(tab) {
+
         streamTabId = tab.id;
+
         console.log(streamTabId);
+
         port.postMessage("started");
+
       });
+
 
     } else if (msg == "stop") {
+
       console.log("stop");
+
       chrome.tabs.remove(streamTabId, function() {
+
         streamTabId = null;
+
         port.postMessage("stopped");
+
       });
+
     } else if (msg == "status") {
+
       if (streamTabId !== null) {
+
         port.postMessage("started");
+
       } else {
+
         port.postMessage("stopped");
+
       }
+
     }
+
   });
+
 });
 
+
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+
   if (tabId == streamTabId) {
+
     console.log("dangit");
+
+  }
+
+});
+
+var toggle = false;
+chrome.browserAction.onClicked.addListener(function(tab) {
+  toggle = !toggle;
+  if(toggle){
+    chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {file:"SCRIPT.user.js"});
+  }
+  else{
+    chrome.browserAction.setIcon({path: "off.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {code:"alert()"});
   }
 });
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,28 +1,52 @@
 var running = false;
 
+
 function startup() {
+
   var runningButton = document.getElementById("runningButton");
+
   var port = chrome.extension.connect({
+
       name: "Background Communication"
+
     });
+
   port.postMessage("status");
-  runningButton.addEventListener('click', function(ev){
+
+  runningButton.addEventListener('click', function(tab){
+
       if (running) {
+
         port.postMessage("stop");
+
       } else {
+
         port.postMessage("start");
+
       }
+
     }, false);
 
+
   port.onMessage.addListener(function(msg) {
+
     if (msg == "stopped") {
-      runningButton.innerHTML = "Start";
+
+      //runningButton.innerHTML = "Start Recording";
+
       running = false;
+
     } else if (msg == "started") {
-      runningButton.innerHTML = "Stop"
+
+      //runningButton.innerHTML = "Stop Recording"
+
       running = true;
+
     }
+
   });
+
 }
 
-window.onload = startup;
+
+window.onload = startup
