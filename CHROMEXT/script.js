5d1f435e9ee8e8e6aab05eec5183f06035531e04
Peter Whalley <peter.a.whalley@gmail.com>
2019-01-27 02:36:09 +0000

    .DS_Store

--- CHROMEXT/script.js
+++ CHROMEXT/script.js
@@ -1,11 +1,17 @@
-function req() {
-  console.log('hello');
-  fetch('https://ichack.org/')
-    .then(function(response) {
-      console.log(response);
-      return response.json()
-    })
-    .then(function (data){
-      console.log('the data', data)
-    });
-}
+console.log('hello');
+
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('link');
+  // onClick's logic below:
+  link.addEventListener('click', function() {
+    console.log('clicking');
+    fetch('http://146.169.200.26/', {mode:"no-cors"})
+      .then(function(response) {
+        console.log(response);
+        link.innerHTML = "got response!";
+      })
+      .then(function (data){
+        link.innerHTML = "got response!";
+      });
+  });
+});
